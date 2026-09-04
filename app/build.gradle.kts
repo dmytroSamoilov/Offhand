23f4a1dea59fe8337337da17562e7061ea027ef3
dmytroSamoilov <dmytro.samoilov.dev@gmail.com>
2026-09-04 15:28:28 +0100

    Version UP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
 
     defaultConfig {
         applicationId = "com.dmytrosamoilov.offhand"
-        versionCode = 14
-        versionName = "1.2.1"
+        versionCode = 15
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
